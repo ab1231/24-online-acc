3b18e75ac1c4cb3ddec6500fc159787b9e39476a
ab1231 <58433211+ab1231@users.noreply.github.com>
2019-12-02 20:26:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Minecraft :(`,"c:/PROGRAM FILES/JAVA/JRE1.8.0_231/BIN/JAVAW.EXE")
+client.user.setGame(`Life is hard :(`,"http://twitch.tv/Tomas<3")
 client.user.setStatus("dnd")
 });
 
